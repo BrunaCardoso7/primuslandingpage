7b694825e2836ffb3e57cd89e787b34722e2c3e4
BrunaCardoso7 <obitadrawing@gmail.com>
2023-09-21 20:17:23 -0300

    feat: sessão pricipal

---
+++ src/app/components/Btn-local.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/btn-local.scss'
+interface ButonLocProps {
+
+}
+
+const ButonLoc: React.FC<ButonLocProps> = ({  }) => {
+    return (
+        <>
+            <button className='button'>Localização</button>
+        </>
+    );
+};
+
+export default ButonLoc;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Header } from './components/Header'
-
+import ButonLoc from './components/Btn-local'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Header/>
-        <div className={styles.imageContainer}>
+    <>
+      <main className={styles.main}>
+        <Header/>
+        <h1>Orçamento que cabe no seu bolso</h1>
+        <p>Fazemos troca de filtro de combustível, filtro de cabine entre outros serviços, para mais informações nos acione no whatsApp.</p>
+        <div className={styles['banner-wrapper']}>
+          <div className={styles['banner-text']}>
+            <span >Troca de óleo</span>
+            <span>+Troca de filtro</span>
+            <span>+Mão de obra</span>
+          </div>
+          <div className={styles['banner-ticket-preco']}>
+            <span className={styles['text-span']}>A partir: </span>
+            <div className={styles["preco"]}>
+              <span className={styles['wrapper-120']}>120,</span>
+              <span className={styles['wrapper-00']}>00</span></div>
+            </div>
         </div>
+        <ButonLoc/>
+      </main>
+      <section>
+        <h1>ola bom dia</h1>
+      </section>
+    </>
 
-    </main>
   )
 }
